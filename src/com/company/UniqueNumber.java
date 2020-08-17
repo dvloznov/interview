f510d48a4464c544585e3388e9d09c0ef2c41aaf
Loznov Denis <Loznov.D.Vas@sberbank.ru>
2020-08-17 14:26:56 +0300

    + Change namings

---
+++ src/com/company/UniqueNumber.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class UniqueNumber {
+
+    public static void main(String... args) {
+        System.out.println(findUniqueNumber(new int[]{1, 1, 3, 4, 4}));
+        System.out.println(findUniqueNumber(new int[]{5, 5, 3, 4, 3}));
+        System.out.println(findUniqueNumber(new int[]{1, 1, 3, 3, 8}));
+    }
+
+    public static int findUniqueNumber(int[] nums) {
+        return 0;
+    }
+}
